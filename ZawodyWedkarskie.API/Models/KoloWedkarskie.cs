e8ad1823d44b34bc9fe31873a6fdb9bcf767ca45
Bartłomiej Cieślak <cieslak.bartlomiej@gmail.com>
2020-03-13 20:46:38 +0100

    Usunięcie niepotrzebnych plików

--- ZawodyWedkarskie.API/Models/KoloWedkarskie.cs
+++ ZawodyWedkarskie.API/Models/KoloWedkarskie.cs
@@ -5,7 +5,8 @@ namespace ZawodyWedkarskie.API.Models
     public class KoloWedkarskie
     {
         [Key]
-        public int Id { get; set; }     
+        public int Id { get; set; }    
+        [Required] 
         public string Nazwa { get; set; }
     }
 }
